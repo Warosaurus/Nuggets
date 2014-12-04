74a2680331bc22683c7bfa571e1edcc176fbbe5a
Warosaurus <warwickbrettlouw@gmail.com>
2014-12-04 02:08:24 +0100

    Still restructuring

--- Docker/build/ubuntuPython/ubuntuPython.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# @Author Warosaurus
-
-#build
-docker build -t ubuntu/temppython .
-containerid=$(docker run -d ubuntu/temppython /bin/bash exit)
-docker export $containerid | docker import - ubuntu:python
-docker rm $containerid
-docker rmi ubuntu/temppython
