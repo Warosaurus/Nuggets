b969f49dca10dd1835c5e30dee9d913b0a38c51f
Warosaurus <warwickbrettlouw@gmail.com>
2014-12-07 22:54:42 +0100

    Finished prototype of algorithm. More work needed but pending information.

--- Apps/a.py
+++ Apps/a.py
@@ -16,9 +16,16 @@ if __name__== "__main__":
 	mem = np.array([int(l.split('= ')[1].split(' ')[0]) for l in open("snmpdump.txt","r") if re.match("HOST-RESOURCES-MIB::hrSWRunPerfMem", l)])
 	print "cpu size: {}".format(len(cpu))
 	print "mem size: {}".format(len(mem))
+	#Find zscores of each value relative to the mean and std. dev.
 	zcpu = stats.zscore(cpu)
 	zmem = stats.zscore(mem)
-	#Graph
+	#Get the position of the values where the abs. zscore is greater than 2.5 (~97%)
+	hcpu = np.array([i for i,n in enumerate(zcpu) if (np.absolute(n) >= 2.5)])
+	hmem = np.array([i for i,n in enumerate(zmem) if (np.absolute(n) >= 2.5)])
+	#Get where the values for each catagory are within the ~97%
+	#spike_pos = [n for n in hcpu,hmem if hcpu[n] == hmem[n]]
+	spikes = [x for x,y in zip(hcpu, hmem) if x == y]
+	#Graph the zscore for each value.
 	fig = Figure()
 	gcpu = fig.add_subplot(1, 1, 1)
 	gmem = fig.add_subplot(2, 1, 1)
