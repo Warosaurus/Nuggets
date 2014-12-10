22231f267f80a8aeb2b2e9ff11e9ca8f17d9d393
Warosaurus <warwickbrettlouw@gmail.com>
2014-12-10 15:59:50 +0100

    *Fixed* ubuntu:localbase not cloning repo. into correct dir.

--- Docker/build.sh
+++ Docker/build.sh
@@ -2,7 +2,9 @@
 #@Author Warosaurus
 #This file will build all the necessary images to create the containers for the infrastructure.
 
-ubuntuLocal/ubuntulocal.sh
-ubuntuPython/ubuntuPython.sh
-ubuntuSQLite/ubuntuSQLite.sh
-echo build complete.
+cd ubuntuLocal
+./ubuntulocal.sh
+cd ../ubuntuPython
+./ubuntuPython.sh
+cd ../ubuntuSQLite
+./ubuntuSQLite.sh
