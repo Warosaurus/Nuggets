7c9e7135e6949530484a91933d38c96ec334764d
Warosaurus <warwickbrettlouw@gmail.com>
2014-12-10 18:34:53 +0100

    Created db volume in container ubuntu:sqlite and added link to container ubuntu:python, updated run script.

--- Docker/run.sh
+++ Docker/run.sh
@@ -2,6 +2,6 @@
 #@Author Warosaurus
 #This file will run all the containers.
 
-docker run -d ubuntu:sqlite
-docker run -d ubuntu:python
+docker run -d --name db ubuntu:sqlite /bin/bash
+docker run -d --volumes-from db ubuntu:python python ~/Nuggets/Apps/a.py
 #To add webapp.
