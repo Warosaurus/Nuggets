02c8e36a3311df074eb590526631bbd4f3aeccb2
Warosaurus <warwickbrettlouw@gmail.com>
2014-12-11 03:11:58 +0100

    Having trouble linking files..

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,7 +1,7 @@
-#!bin/bash
+#!/bin/bash
 #@Author Warosaurus
 #This file will run all the containers.
 
-docker run -d --name db ubuntu:sqlite /bin/bash
-docker run -d --volumes-from db ubuntu:python python ~/Nuggets/Apps/a.py
+docker run -d -t -P --name db -v /home/indi/:/home/local/ ubuntu:sqlite /bin/bash
+docker run -d -t -i --volumes-from db --name py ubuntu:python python /home/Nuggets/Apps/a.py
 #To add webapp.
